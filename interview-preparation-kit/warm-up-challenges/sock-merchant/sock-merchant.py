26b6c40b61d06208dc3387cbc8728f77c91a7d51
jesux <jj.reyesquevedo@gmail.com>
2020-02-15 17:25:08 -0600

    move to warm up challenges

---
+++ interview-preparation-kit/warm-up-challenges/sock-merchant/sock-merchant.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import collections
+
+# Complete the sockMerchant function below.
+# Solution to  https://www.hackerrank.com/challenges/sock-merchant/problem
+def sockMerchant(n, ar):
+    totalPairs = 0
+    counter = collections.Counter(ar)
+
+    for color in counter:
+        numColorSocks=(counter[color])   
+        totalPairs += numColorSocks // 2
+
+    return totalPairs
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
