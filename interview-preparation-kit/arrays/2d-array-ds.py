899332edd0f715c2cece640b6f044c3e7b2e224f
jesux <jj.reyesquevedo@gmail.com>
2020-02-24 21:27:28 -0600

    My solution to https://www.hackerrank.com/challenges/2d-array/problem

---
+++ interview-preparation-kit/arrays/2d-array-ds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+# Solution to https://www.hackerrank.com/challenges/2d-array/problem
+
+def hourglassSum(arr):
+    sums = []
+    for x in list(range(4)):
+        for y in list(range(4)):
+            center = arr[x+1][y+1]; 
+            up = arr[x][y+1];  upL = arr[x][y]; upR = arr[x][y+2]; 
+            down = arr[x+2][y+1]; dowL = arr[x+2][y]; downR = arr[x+2][y+2]; 
+            sums.append(center+up+upL+upR+down+dowL+downR)
+    return max(sums)
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+    print(result)
+
+    #fptr.write(str(result) + '\n')
+
+    #fptr.close()
